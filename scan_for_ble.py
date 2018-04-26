9e211dfde26c65bfa22f4772a96ea5b82923d5e5
Simon Alexander Alsing <aalsing@gmail.com>
2018-04-26 09:54:23 +0000

    Add ble scan for devices python file

---
+++ scan_for_ble.py
@@ -0,0 +1,10 @@
+
+
+# bluetooth low energy scan
+from bluetooth.ble import DiscoveryService
+
+service = DiscoveryService("hci0")
+devices = service.discover(2)
+
+for address, name in devices.items():
+    print("name: {}, address: {}".format(name, address))
