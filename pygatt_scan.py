4676250321d59bb38d0d2f6c60dcb21a53b0d563
Simon Alexander Alsing <aalsing@gmail.com>
2018-05-25 14:18:17 +0000

    Got a working connection with the hm10

--- pygatt_scan.py
+++ pygatt_scan.py
@@ -1,21 +1,23 @@
-# Implementation taken from https://github.com/peplin/pygatt
+# Implementation inspired from https://github.com/peplin/pygatt
 
 import pygatt
 import logging
+import time
+
 from binascii import hexlify
 
 logging.basicConfig()
 logging.getLogger('pygatt').setLevel(logging.DEBUG)
 
-def handle_data(handle, value):
+def handle_data(handle: int, value: bytearray):
     """
         handle -- integer, characteristic read handle the data was received on
         value -- bytearray, the data returned in the notification
-        """
+    """
     print("Received data: %s" % hexlify(value))
 
 
-hm10_uuid = ""
+hm10_uuid = "0000FFE1-0000-1000-8000-00805F9B34FB"
 hm10_address = "D4:36:39:BB:E8:D6"
 # The BGAPI backend will attemt to auto-discover the serial device name of the
 # attached BGAPI-compatible USB adapter.
@@ -26,7 +28,9 @@ try:
     device = adapter.connect(address=hm10_address)
     val = device.discover_characteristics()
     print("discover_characteristics", val)
-    # value = device.char_read(hm10_uuid)
-    # device.subscribe(hm10_uuid, callback=handle_data)
+    device.subscribe(hm10_uuid, callback=handle_data)
+    while True:
+        time.sleep(1)
+
 finally:
     adapter.stop()
